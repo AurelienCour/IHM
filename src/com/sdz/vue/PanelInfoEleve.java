3e781ebfffe613a443486c7f04f805f6f7fc6e0f
Aurelien <Aurelien@192.168.1.12>
2017-03-24 16:59:09 +0100

    Amélioration affichage eleve

--- src/com/sdz/vue/PanelInfoEleve.java
+++ src/com/sdz/vue/PanelInfoEleve.java
@@ -1,6 +1,7 @@
 package com.sdz.vue;
 
 import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,19 +11,36 @@ import com.sdz.model.Eleve;
 
 public class PanelInfoEleve extends JPanel{
 
-	private JLabel labelInfoEnfant;
+	private JLabel imageEnfant;
+	private JLabel nomEnfant;
+	private JLabel prenomEnfant;
+	private JLabel age;
+	private JLabel sexe;
+	private Font police = new Font("Serif", Font.PLAIN, 20);
 	
 	public PanelInfoEleve(){
-		Font police = new Font("Serif", Font.PLAIN, 50);
-		
-		labelInfoEnfant = new JLabel();
-		labelInfoEnfant.setFont(police);
-		labelInfoEnfant.setHorizontalAlignment(SwingConstants.CENTER);
-		labelInfoEnfant.setVerticalAlignment(SwingConstants.CENTER);
-		this.add(labelInfoEnfant);
+		this.setLayout(new GridLayout(5,1));
+		imageEnfant = new JLabel("");
+		imageEnfant.setFont(police);
+		nomEnfant = new JLabel();
+		nomEnfant.setFont(police);
+		prenomEnfant = new JLabel();
+		prenomEnfant.setFont(police);
+		age = new JLabel();
+		age.setFont(police);
+		sexe = new JLabel();
+		sexe.setFont(police);
+		this.add(imageEnfant);
+		this.add(nomEnfant);
+		this.add(prenomEnfant);
+		this.add(age);
+		this.add(sexe);
 	}
 
 	public void changeInfo(Eleve e) {
-		labelInfoEnfant.setText(e.toString());
+		nomEnfant.setText(e.getNom());
+		prenomEnfant.setText(e.getPrenom());
+		age.setText(""+e.getAge());
+		sexe.setText(""+e.getSexe());
 	}
 }
